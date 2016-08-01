6325305d786cd832772aa01dd096e0bfefe9bec6
Scott Robertson <scottymeuk@gmail.com>
2016-08-01 21:48:50 +0100

    Use python-dotenv

--- python/settings.py
+++ python/settings.py
@@ -1,25 +1,19 @@
 import os
 import logging
 
-def get_env():
-    # Load the settings from the .env file if no environment has been selected
-    if not os.environ.get('ENV'):
-        a = os.path.join(os.path.dirname(__file__), '../.env')
-        with open(a) as f:
-            defaults = dict(tuple(x.strip().split('=')) for x in f.readlines() if x != '\n')
-            os.environ.update(defaults)
+from os.path import join, dirname
+from dotenv import load_dotenv
 
-    return os.environ
+dotenv_path = join(dirname(dirname(__file__)), '.env')
+load_dotenv(dotenv_path)
 
-env = get_env()
-
-log_level = env.get('LOG_LEVEL').upper()
+log_level = os.environ.get('LOG_LEVEL').upper()
 logging.basicConfig(level=getattr(logging, log_level))
 flask_debug = True if log_level == 'debug' else False
 
-ynab_account = env.get('YNAB_ACCOUNT')
-ynab_budget = env.get('YNAB_BUDGET')
-ynab_username = env.get('YNAB_USERNAME')
-ynab_password = env.get('YNAB_PASSWORD')
+ynab_account = os.environ.get('YNAB_ACCOUNT')
+ynab_budget = os.environ.get('YNAB_BUDGET')
+ynab_username = os.environ.get('YNAB_USERNAME')
+ynab_password = os.environ.get('YNAB_PASSWORD')
 
-sentry_dsn = env.get('SENTRY_DSN')
+sentry_dsn = os.environ.get('SENTRY_DSN')
