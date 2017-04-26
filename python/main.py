dc38e291654729d7a0cf072d8281794dbd5abd8c
Scott Robertson <scottymeuk@gmail.com>
2017-04-26 14:01:01 +0100

    Define a port
    Potentially fix #13

--- python/main.py
+++ python/main.py
@@ -134,4 +134,5 @@ def route_webhook():
     return ''
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
