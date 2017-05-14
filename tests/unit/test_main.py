6b6b1020e57ed15c5f9120582636e2c1e97c9202
Scott Robertson <scottymeuk@gmail.com>
2017-05-14 08:58:38 +0100

    Remove test as we don't have this method now

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -1,5 +1,4 @@
 from unittest import TestCase
-from python.main import get_payee_details_for_transaction
 from python.main import get_subcategory_id_for_transaction
 from python.main import get_p2p_transaction_payee_name
 
@@ -9,20 +8,6 @@ class Stub(object):
 
 
 class TestMain(TestCase):
-    def test_get_payee_details_for_transaction(self):
-        transaction_stub = Stub()
-        payee_stub = Stub()
-        subcategory_stub = Stub()
-
-        payee_stub.id = '123'
-        subcategory_stub.id = '234'
-        subcategory_stub.name = 'Test'
-        transaction_stub.entities_payee = payee_stub
-        transaction_stub.entities_subcategory = subcategory_stub
-
-        self.assertTupleEqual(get_payee_details_for_transaction(transaction_stub, 'Test Payee Name'),
-                              ('123', '234'))
-
     def test_get_subcategory_id_for_transaction(self):
         transaction_stub = Stub()
         subcategory_stub = Stub()
