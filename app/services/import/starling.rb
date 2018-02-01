ef5e6cf1b88d7b5734e61f3d09a76ca214ae72cd
Scott Robertson <scottymeuk@gmail.com>
2018-02-01 01:19:04 +0000

    Add from param to Starling import

--- app/services/import/starling.rb
+++ app/services/import/starling.rb
@@ -1,12 +1,14 @@
 class Import::Starling
-  def initialize(access_token, ynab_account_id)
+  def initialize(access_token, ynab_account_id, from: nil)
     @starling = Starling::Client.new(access_token: access_token)
     @ynab_account_id = ynab_account_id
+    @from = from
   end
 
   def import
+    from = (@from || @starling.account.get.created_at).to_date
     transactions_to_create = []
-    @starling.transactions.list(params: {from: @starling.account.get.created_at.to_date, to: Date.today}).each do |transaction|
+    @starling.transactions.list(params: { from: from, to: Date.today }).each do |transaction|
       transactions_to_create << {
         amount: (transaction.amount * 1000).to_i,
         payee_name: transaction.narrative.strip,
