847f54e423608d8ec8cfaf3c9980a781b2ade395
Scott Robertson <scottymeuk@gmail.com>
2018-02-16 20:47:42 +0000

    Batching the bulk endpoints

--- app/services/ynab/bulk_transaction_creator.rb
+++ app/services/ynab/bulk_transaction_creator.rb
@@ -1,28 +1,45 @@
 class YNAB::BulkTransactionCreator
+
+  BATCH_SIZE = 20.freeze
+
   def initialize(transactions, budget_id: nil, account_id: nil)
     @transactions = transactions
     @client = YNAB::Client.new(ENV['YNAB_ACCESS_TOKEN'], budget_id, account_id)
   end
 
   def create
-    transactions_to_create = []
+    batches = (@transactions.size.to_f / BATCH_SIZE).ceil
+    per_batch = @transactions.size / batches
 
-    @transactions.each do |transaction|
-      transactions_to_create << {
-        import_id: transaction[:id],
-        payee_name: transaction[:payee_name],
-        amount: transaction[:amount],
-        memo: transaction[:description],
-        date: transaction[:date].to_date,
-        cleared: !!transaction[:cleared] ? 'Cleared' : 'Uncleared',
-        flag: transaction[:flag]
-      }
-    end
+    Rails.logger.info("Splitting #{@transactions.size} transactions into #{batches} batches")
 
-    if transactions_to_create.any?
-      @client.create_transactions(transactions_to_create)
-    else
-      :no_transactions_to_create
+    @transactions.each_slice(BATCH_SIZE).with_index do |transactions, index|
+      
+      Rails.logger.info("Processing batch #{index + 1} of #{batches}")
+
+      transactions_to_create = []
+      transactions.each do |transaction|
+
+        transactions_to_create << {
+          import_id: transaction[:id],
+          payee_name: transaction[:payee_name],
+          amount: transaction[:amount],
+          memo: transaction[:description],
+          date: transaction[:date].to_date,
+          cleared: !!transaction[:cleared] ? 'Cleared' : 'Uncleared',
+          flag: transaction[:flag]
+        }
+      end
+
+      if transactions_to_create.any?
+        begin
+          Rails.logger.info(@client.create_transactions(transactions_to_create))
+        rescue => e
+          Rails.logger.error(e.response.body)
+        end
+      else
+        Rails.logger.info(:no_transactions_to_create)
+      end
     end
   end
 end
