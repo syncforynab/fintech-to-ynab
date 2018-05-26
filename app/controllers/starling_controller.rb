332dc85ec25f6c793af27ce7df4f1da6b02e8586
Scott Robertson <scottymeuk@gmail.com>
2018-05-26 20:05:41 +0100

    Add basic support for card payments, and faster payments
    Need to find a list of webhook types
    
    Closes #23

--- app/controllers/starling_controller.rb
+++ app/controllers/starling_controller.rb
@@ -1,6 +1,36 @@
 class StarlingController < ApplicationController
   def receive
-    # @todo once starling support personal webhooks, add this support
-    render json: { saved: false }
+    webhook = JSON.parse(request.body.read, symbolize_names: true)
+
+    case webhook[:webhookType]
+    when 'TRANSACTION_CARD', 'TRANSACTION_AUTH_FULL_REVERSAL', 'TRANSACTION_FASTER_PAYMENT_OUT', 'TRANSACTION_FASTER_PAYMENT_IN'
+      payee_name = webhook[:content][:counterParty]
+      amount = (webhook[:content][:amount].to_f * 1000).to_i
+      description = webhook[:content][:forCustomer].to_s
+      flag = nil
+
+      foreign_transaction = webhook[:content][:sourceCurrency] != 'GBP'
+      flag = 'orange' if foreign_transaction
+    else
+      return render json: { error: :unsupported_type }
+    end
+
+    ynab_creator = YNAB::TransactionCreator.new(
+      date: Time.parse(webhook[:timestamp]).to_date,
+      amount: amount,
+      payee_name: payee_name,
+      description: description.strip,
+      cleared: !foreign_transaction,
+      flag: flag,
+      account_id: ENV['YNAB_STARLING_ACCOUNT_ID']
+    )
+
+    create = ynab_creator.create
+    if create.try(:id).present?
+      render json: create
+    else
+      render json: create, status: 400
+    end
+
   end
 end
