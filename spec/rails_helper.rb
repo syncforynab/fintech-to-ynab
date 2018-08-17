29af0ac359931a17e936e7e94703cddcfbd5ebf7
Scott Robertson <scottymeuk@gmail.com>
2018-08-17 23:15:36 +0100

    Style fixes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 
 Dir[Rails.root.join('spec/support/*.rb')].each { |f| require f }
