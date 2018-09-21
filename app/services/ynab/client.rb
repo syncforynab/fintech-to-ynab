916e31d2c3fb746da7bd0ec0c7e538f18d923a35
Scott Robertson <scottymeuk@gmail.com>
2018-09-21 22:39:19 +0100

    Adding update_transaction method

--- app/services/ynab/client.rb
+++ app/services/ynab/client.rb
@@ -17,8 +17,33 @@ class YNAB::Client
     client.categories.get_category_by_id(selected_budget_id, category_id).data.category
   end
 
-  def transactions(since_date: nil)
-    @transactions ||= client.transactions.get_transactions(selected_budget_id, since_date: since_date).data.transactions
+  def transactions(since_date: nil, account_id: nil)
+    if account_id.present?
+      client.transactions.get_transactions_by_account(selected_budget_id, account_id, since_date: since_date).data.transactions
+    else
+      client.transactions.get_transactions(selected_budget_id, since_date: since_date).data.transactions
+    end
+  end
+
+  def update_transaction(transaction_id, transaction)
+    client.transactions.update_transaction(selected_budget_id, transaction_id, {
+      transaction: {
+        account_id: selected_account_id,
+        date: transaction[:date].to_s,
+        amount: transaction[:amount],
+        payee_id: transaction[:payee_id],
+        payee_name: transaction[:payee_name],
+        cleared: transaction[:cleared] ? "Cleared" : 'Uncleared',
+        memo: transaction[:description],
+        flag_color: transaction[:flag],
+        import_id: transaction[:id]
+      }
+    }).data.transaction
+  rescue => e
+    Rails.logger.error('YNAB::Client.update_transaction failure')
+    Rails.logger.error("YNAB::Client.update_transaction Response: #{e.response_body}")
+    Rails.logger.error(e)
+    false
   end
 
   def create_transaction(id: nil, payee_id: nil, payee_name: nil, amount: nil, cleared: nil, date: nil, memo: nil, flag: nil)
