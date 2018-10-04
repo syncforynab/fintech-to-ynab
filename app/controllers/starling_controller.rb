0430eeb980d804eb824b4d12d0ea78715c3a695f
Scott Robertson <scottymeuk@gmail.com>
2018-10-04 23:15:57 +0100

    Some fixes

--- app/controllers/starling_controller.rb
+++ app/controllers/starling_controller.rb
@@ -1,8 +1,8 @@
 class StarlingController < ApplicationController
   def receive
     webhook = JSON.parse(request.body.read, symbolize_names: true)
-    import = ::F2ynab::Webhooks::Starling.new(webhook).import
-    if import[:error]
+    import = ::F2ynab::Webhooks::Starling.new(webhook, ynab_account_id: params[:ynab_account_id]).import
+    if import.try(:id) || import.try(:[], :warning)
       render json: import, status: 400
     else
       render json: import
