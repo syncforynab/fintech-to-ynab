4f53e59c074b437ab41ebfb98060f20a58452474
Scott Robertson <scottymeuk@gmail.com>
2018-10-04 22:58:52 +0100

    Moving webhook logic to library

--- spec/controllers/starling_controller_spec.rb
+++ spec/controllers/starling_controller_spec.rb
@@ -15,18 +15,18 @@ RSpec.describe StarlingController, type: :controller do
       before { allow(ENV).to receive(:[]).with('URL_SECRET').and_return('SECRET') }
       subject { post :receive, body: body.to_json, format: :json, params: { secret: 'SECRET' } }
       it { is_expected.to have_http_status(200) }
-      it { is_expected.to have_json('error' => 'unsupported_type') }
+      it { is_expected.to have_json('warning' => 'unsupported_type') }
     end
 
     context 'when sending no body' do
       it { is_expected.to have_http_status(200) }
-      it { is_expected.to have_json('error' => 'unsupported_type') }
+      it { is_expected.to have_json('warning' => 'unsupported_type') }
     end
 
     context 'when sending an unsupported webhook type' do
       let(:body) { { type: :not_supported } }
       it { is_expected.to have_http_status(200) }
-      it { is_expected.to have_json('error' => 'unsupported_type') }
+      it { is_expected.to have_json('warning' => 'unsupported_type') }
     end
   end
 end
