380ac481588aab96603e466963df13483b61b394
Scott Robertson <scottymeuk@gmail.com>
2018-10-08 01:19:39 +0100

    Passing the YNAB client in

--- app/controllers/monzo_controller.rb
+++ app/controllers/monzo_controller.rb
@@ -3,8 +3,9 @@ class MonzoController < ApplicationController
     webhook = JSON.parse(request.body.read, symbolize_names: true)
 
     ynab_account_id = params[:ynab_account_id] || ENV['YNAB_MONZO_ACCOUNT_ID'] || ENV['YNAB_ACCOUNT_ID']
+    ynab_client = ::F2ynab::YNAB::Client.new(ENV['YNAB_ACCESS_TOKEN'], ENV['YNAB_BUDGET_ID'], ynab_account_id)
 
-    import = ::F2ynab::Webhooks::Monzo.new(webhook, ynab_account_id,
+    import = ::F2ynab::Webhooks::Monzo.new(ynab_client, webhook,
       skip_emoji: ENV['SKIP_EMOJI'].present?,
       skip_tags: ENV['SKIP_TAGS'].present?,
       skip_foreign_currency_flag: ENV['SKIP_FOREIGN_CURRENCY_FLAG'].present?,
--- app/controllers/starling_controller.rb
+++ app/controllers/starling_controller.rb
@@ -3,8 +3,9 @@ class StarlingController < ApplicationController
     webhook = JSON.parse(request.body.read, symbolize_names: true)
 
     ynab_account_id = params[:ynab_account_id] || ENV['YNAB_STARLING_ACCOUNT_ID']
+    ynab_client = ::F2ynab::YNAB::Client.new(ENV['YNAB_ACCESS_TOKEN'], ENV['YNAB_BUDGET_ID'], ynab_account_id)
 
-    import = ::F2ynab::Webhooks::Starling.new(webhook, ynab_account_id,
+    import = ::F2ynab::Webhooks::Starling.new(ynab_client, webhook,
       skip_foreign_currency_flag: ENV['SKIP_FOREIGN_CURRENCY_FLAG'].present?,
     ).import
 
