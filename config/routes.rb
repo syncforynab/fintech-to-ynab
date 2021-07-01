ecbbe3fa4d47a3222b1dc3d189339c689b8f4ff8
Gaz Franklin <72508417+GazFranklin@users.noreply.github.com>
2021-07-01 08:59:56 +0100

    Starling V2 Webhook support (#310)

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/ping', to: 'application#ping'
   post '/monzo', to: 'monzo#receive'
   post '/starling', to: 'starling#receive'
+  post '/starling/feed-item', to: 'starlingv2#feed'
 end
