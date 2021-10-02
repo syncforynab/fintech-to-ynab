713ba4b20428c8465da086eefa2e04d50f3e3334
Adam Warner <me@adamwarner.co.uk>
2021-10-02 23:54:05 +0100

    Prevent "Blocked host:" message  (#336)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Prevent "Blocked host: hostname"
+  if ENV["APP_HOSTNAME"].present?
+    config.hosts << ENV["APP_HOSTNAME"]
+  end
+
 end
